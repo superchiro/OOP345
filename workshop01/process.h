d22d4690977fbd1da5edfe07965ca0599f5cc25d
superchiro <85416957+superchiro@users.noreply.github.com>
2022-03-15 05:09:47 -0400

    Add files via upload

---
+++ workshop01/process.h
@@ -0,0 +1,17 @@
+// Name: Chihiro Kawaguchi
+// Seneca Student ID: 127694180
+// Seneca email: ckawaguchi@myseneca.ca
+// Date of completion: 2019/09/15
+//
+// I confirm that the content of this file is created by me,
+//   with the exception of the parts provided to me by my professor.
+
+#ifndef INC_3RDSEMESTER_PROCESS_H
+#define INC_3RDSEMESTER_PROCESS_H
+
+namespace sdds
+{
+    void process(const char* str);
+}
+
+#endif //INC_3RDSEMESTER_PROCESS_H
